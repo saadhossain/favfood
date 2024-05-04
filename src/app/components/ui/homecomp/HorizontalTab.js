6eb33054c492bf87631891df497f0291a6e7c459
saadhossain <mshsarkar@gmail.com>
2024-05-05 04:54:40 +0600

    Horizonal tab convert to slider

--- src/app/components/ui/homecomp/HorizontalTab.js
+++ src/app/components/ui/homecomp/HorizontalTab.js
@@ -5,28 +5,67 @@ import { BiSolidDish, BiSolidOffer } from "react-icons/bi";
 import { FaAward, FaStar } from "react-icons/fa";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { MdFiberNew } from "react-icons/md";
+import Slider from 'react-slick';
 import FoodData from './FoodData';
 const HorizontalTab = () => {
-    const tabStyle = 'hover:text-secondary border-b-4 hover:border-secondary pb-2 duration-200 ease-in-out flex items-center gap-1 cursor-pointer min-w-36';
-    const { tabQuery, setTabQuery, loading, setLoading } = useContext(DataContext);
+    const tabStyle = 'horizontal-tab hover:text-secondary border-b-4 hover:border-secondary pb-2 duration-200 ease-in-out cursor-pointer min-w-36';
+    const { tabQuery, setTabQuery, loading } = useContext(DataContext);
 
-    // console.log(tabQuery);
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 5,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 5
+                }
+            }
+        ]
+    };
     return (
-        <div className='mt-3 md:mt-12 min-h-[350px] relative'>
-            <ul className='flex gap-2 items-center justify-between overflow-x-auto'>
-                <li className={`${tabStyle} ${tabQuery === 'all-food' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('all-food')}><BiSolidDish className='w-5 h-5' />All Food</li>
-                <li className={`${tabStyle} ${tabQuery === 'promotions' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('promotions')}><BiSolidOffer className='w-5 h-5' />Promotions</li>
-                <li className={`${tabStyle} ${tabQuery === 'newcomers' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('newcomers')}><MdFiberNew className='w-5 h-5' />Newcomers</li>
-                <li className={`${tabStyle} ${tabQuery === 'best-seller' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('best-seller')}><FaAward className='w-5 h-5' />Best Sellers</li>
-                <li className={`${tabStyle} ${tabQuery === 'top-rated' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('top-rated')}><FaStar className='w-5 h-5' />Top Rated</li>
-            </ul>
-            <hr className='border-b-4 border-[#d9d9d9] -mt-1' />
+        <div className='mt-3 md:mt-12 min-h-[350px] relative slider-container'>
+            <Slider {...settings}>
+                <div className={`${tabStyle} ${tabQuery === 'all-food' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('all-food')}><BiSolidDish className='w-5 h-5' />All Food</div>
+                <div className={`${tabStyle} ${tabQuery === 'promotions' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('promotions')}><BiSolidOffer className='w-5 h-5' />Promotions</div>
+                <div className={`${tabStyle} ${tabQuery === 'newcomers' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('newcomers')}><MdFiberNew className='w-5 h-5' />Newcomers</div>
+                <div className={`${tabStyle} ${tabQuery === 'best-seller' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('best-seller')}><FaAward className='w-5 h-5' />Best Seller</div>
+                <div className={`${tabStyle} ${tabQuery === 'top-rated' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('top-rated')}><FaStar className='w-5 h-5' />Top Rated</div>
+                {/* <ul className='flex gap-2 items-center justify-between overflow-x-auto'>
+                    <li className={`${tabStyle} ${tabQuery === 'all-food' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('all-food')}><BiSolidDish className='w-5 h-5' />All Food</li>
+                    <li className={`${tabStyle} ${tabQuery === 'promotions' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('promotions')}><BiSolidOffer className='w-5 h-5' />Promotions</li>
+                    <li className={`${tabStyle} ${tabQuery === 'newcomers' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('newcomers')}><MdFiberNew className='w-5 h-5' />Newcomers</li>
+                    <li className={`${tabStyle} ${tabQuery === 'best-seller' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('best-seller')}><FaAward className='w-5 h-5' />Best Sellers</li>
+                    <li className={`${tabStyle} ${tabQuery === 'top-rated' ? 'text-secondary border-secondary' : 'text-black border-gray-100'}`} onClick={() => setTabQuery('top-rated')}><FaStar className='w-5 h-5' />Top Rated</li>
+                </ul> */}
+            </Slider>
+            <hr className='border-b-4 border-gray-100 -mt-1' />
             <FoodData tabQuery={tabQuery} loading={loading} />
 
             {/* Arrow Buttons */}
-            <div className='w-full flex justify-between absolute top-0 -z-50 text-primary md:hidden'>
-                <IoIosArrowBack className='-ml-5 h-6 w-6'/>
-                <IoIosArrowForward className='-mr-5 h-6 w-6'/>
+            <div className='w-full justify-between absolute top-0 -z-50 text-primary hidden'>
+                <IoIosArrowBack className='-ml-5 h-6 w-6' />
+                <IoIosArrowForward className='-mr-5 h-6 w-6' />
             </div>
         </div>
     );
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import HeaderNavigation from './components/shared/HeaderNavigation';
 import DataProvider from './context/DataContext';
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const inter = Fira_Code({
   subsets: ["latin"],
