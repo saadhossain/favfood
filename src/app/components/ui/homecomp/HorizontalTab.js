fa14879ec6fe65a58947d129a17b605d643d45d6
saadhossain <mshsarkar@gmail.com>
2024-05-05 03:55:32 +0600

    New icon changed

--- src/app/components/ui/homecomp/HorizontalTab.js
+++ src/app/components/ui/homecomp/HorizontalTab.js
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { BiSolidDish, BiSolidOffer } from "react-icons/bi";
 import { FaAward, FaStar } from "react-icons/fa";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { MdFiberNew } from "react-icons/md";
 import FoodData from './FoodData';
 const HorizontalTab = () => {
     const tabStyle = 'hover:text-secondary border-b-4 hover:border-secondary pb-2 duration-200 ease-in-out flex items-center gap-1 cursor-pointer min-w-36';
@@ -15,7 +16,7 @@ const HorizontalTab = () => {
             <ul className='flex gap-2 items-center justify-between overflow-x-auto'>
                 <li className={`${tabStyle} ${tabQuery === 'all-food' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('all-food')}><BiSolidDish className='w-5 h-5' />All Food</li>
                 <li className={`${tabStyle} ${tabQuery === 'promotions' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('promotions')}><BiSolidOffer className='w-5 h-5' />Promotions</li>
-                <li className={`${tabStyle} ${tabQuery === 'newcomers' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('newcomers')}><BiSolidDish className='w-5 h-5' />Newcomers</li>
+                <li className={`${tabStyle} ${tabQuery === 'newcomers' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('newcomers')}><MdFiberNew className='w-5 h-5' />Newcomers</li>
                 <li className={`${tabStyle} ${tabQuery === 'best-seller' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('best-seller')}><FaAward className='w-5 h-5' />Best Sellers</li>
                 <li className={`${tabStyle} ${tabQuery === 'top-rated' ? 'text-secondary border-secondary' : 'text-black border-[#d9d9d9]'}`} onClick={() => setTabQuery('top-rated')}><FaStar className='w-5 h-5' />Top Rated</li>
             </ul>
