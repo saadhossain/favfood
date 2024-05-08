40fb5315fc4bcdbfd93a714422b09b4bc72edb5e
saadhossain <mshsarkar@gmail.com>
2024-05-09 01:41:29 +0600

    Make Cart Page responsive

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -19,10 +19,10 @@ const CartPage = () => {
     //Calculate total price of all product in the cart
     const totalPrice = getTotalPrice();
     return (
-        <div className='w-11/12 md:w-10/12 mx-auto my-10 flex gap-10 justify-between'>
+        <div className='w-11/12 md:w-10/12 mx-auto my-3 md:my-10 md:flex gap-10 justify-between'>
             {/* Product Details */}
             <div className='w-full md:w-9/12'>
-                <h2 className='text-2xl font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Shopping Cart</h2>
+                <h2 className='text-xl md:text-2xl font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Shopping Cart</h2>
                 {
                     (loading || !productsInCart) ? <CartItemsLoader /> : <ProductsTable
                         productsInCart={productsInCart}
@@ -32,7 +32,7 @@ const CartPage = () => {
             </div>
             {/* Cart Calculation */}
             <div className='w-full md:w-3/12'>
-                <h3 className='text-xl font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Cart Summery</h3>
+                <h3 className='text-lg mt-3 md:mt-0 md:text-xl font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Cart Summery</h3>
                 {
                     (loading || !productsInCart) ? <CartSummeryLoader /> : <CartSummery totalPrice={totalPrice} />
                 }
