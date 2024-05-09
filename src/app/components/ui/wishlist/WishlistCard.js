b297c4f7426b23e68f6b03175dc897f763ac3b14
saadhossain <mshsarkar@gmail.com>
2024-05-09 20:05:42 +0600

    Wishlist page make responsive

--- src/app/components/ui/wishlist/WishlistCard.js
+++ src/app/components/ui/wishlist/WishlistCard.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { DataContext } from '@/app/context/DataContext';
 import { useHandleAddToCart } from '@/app/hooks/useHandleAddToCart';
 import Image from 'next/image';
@@ -8,7 +8,7 @@ import { FaCheckCircle } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 
 const WishlistCard = ({ product }) => {
-    const {setWishlistInLocalStorage, setWishlistQuantity} = useContext(DataContext);
+    const { setWishlistInLocalStorage, setWishlistQuantity } = useContext(DataContext);
     const handleAddToCart = useHandleAddToCart();
 
     const handleRemoveProductFromWishlist = (id) => {
@@ -19,24 +19,33 @@ const WishlistCard = ({ product }) => {
         localStorage.setItem('favFoodWishlist', JSON.stringify(updatedProducts));
         setWishlistInLocalStorage(updatedProducts);
         setWishlistQuantity(updatedProducts.length);
-        toast.error('Food removed from Wishlist.')
+        toast.error('Food removed from Wishlist.');
     };
     return (
-        <div className='flex items-center justify-between border-b-2 border-gray-200 py-2 px-5 text-gray-800'>
+        <div className='md:flex items-center justify-between gap-2 bg-gray-100 md:border-b-2 border-gray-200 py-2 px-2 md:px-5 text-gray-800 rounded-md md:rounded-none relative'>
+            {/* default remove button for computer */}
             <button
-            onClick={()=> handleRemoveProductFromWishlist(product._id)}
-            className='text-red-600 font-semibold text-xl'
+                onClick={() => handleRemoveProductFromWishlist(product._id)}
+                className='text-red-600 font-semibold text-xl hidden md:block'
             >X</button>
-            <Image src={product.image} alt={product.name} width={60} height={40} className='rounded-md' />
-            <h4>{product.name}</h4>
-            <h5>${product.price}</h5>
-            <div className='flex gap-1 items-center text-green-600'>
-                <FaCheckCircle />
-                In Stock
-            </div>
+            {/* //Remove button for mobile */}
             <button
-                onClick={() => handleAddToCart(product?._id)}
-                className='flex gap-2 items-center bg-primary text-white py-2 px-5 rounded-md my-3 hover:bg-secondary duration-300 ease-in-out'><FaCartShopping />Add to Cart</button>
+                onClick={() => handleRemoveProductFromWishlist(product._id)}
+                className='text-red-600 font-semibold text-xl absolute top-3 left-3 bg-gray-100 py-1 px-3 rounded-full md:hidden'
+            >X</button>
+            <Image src={product?.image} alt={product.name} width={60} height={40} className='w-full md:w-1/12 rounded-md' />
+            <h4 className='w-full md:w-6/12'>{product.name}</h4>
+            {/* Price, Stock and Action Button */}
+            <div className='w-full md:w-5/12 flex gap-2 items-center'>
+                <h5 className='w-3/4 md:w-2/12 font-semibold'>${product.price}</h5>
+                <div className='w-4/12 md:flex gap-1 items-center text-green-600 hidden'>
+                    <FaCheckCircle />
+                    In Stock
+                </div>
+                <button
+                    onClick={() => handleAddToCart(product?._id)}
+                    className='w-1/4 md:w-6/12 flex gap-1 items-center bg-primary text-white py-2 px-2 md:px-3 rounded-md my-3 hover:bg-secondary duration-300 ease-in-out'><FaCartShopping /><span className='hidden md:block'>Add to Cart</span></button>
+            </div>
         </div>
     );
 };
--- src/app/wishlist/page.js
+++ src/app/wishlist/page.js
@@ -11,22 +11,20 @@ const Wishlist = () => {
     const { loading, setWishlistInLocalStorage } = useContext(DataContext);
     // Get products from localstorage and set them to setWishlistInLocalStorage state
     getProductsFromLocalStorage('favFoodWishlist', setWishlistInLocalStorage);
-
     //Get all wishlist products in the cart
     const productsInWishlist = getProductsInWishlist();
     return (
         <div className='w-11/12 md:w-10/12 mx-auto my-3 md:my-10 md:flex gap-10 justify-between'>
             {/* Wishlist Items */}
-            <div className='w-full md:w-9/12'>
+            <div className='w-full md:w-10/12'>
                 <h2 className='text-xl md:text-2xl font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Wishlist</h2>
                 {
-                    (loading || !productsInWishlist) ? <WishlistItemsloader/> : <div className='bg-gray-100 rounded-md'>
+                    (loading || !productsInWishlist) ? <WishlistItemsloader/> : <div className='rounded-md grid grid-cols-2 md:block gap-2 md:gap-0'>
                         {
                             productsInWishlist?.map((product)=> <WishlistCard key={product?._id} product={product}/>)
                         }
                     </div>
                 }
-                {/* <WishlistItemsloader/> */}
             </div>
         </div>
     );
