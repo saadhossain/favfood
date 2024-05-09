75875e3d401de64e2639f85845fcba54069fbe28
saadhossain <mshsarkar@gmail.com>
2024-05-10 01:10:10 +0600

    Login, Register page make responsive

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,24 +3,24 @@ import LoginBg from '/public/login-bg.jpg'
 import Link from 'next/link';
 const LoginPage = () => {
     return (
-        <div className='w-10/12 mx-auto my-10 flex justify-center'>
-            <Image src={LoginBg} alt='Login BG' className='rounded-l-md'/>
-            <div className="w-full flex flex-col max-w-md p-6 rounded-r-md sm:p-10 bg-white text-gray-900 shadow-2xl">
-                <div className="mb-8 text-center">
-                    <h1 className="my-3 text-4xl font-bold">Login</h1>
+        <div className='w-11/12 md:w-10/12 mx-auto my-5 md:my-10 flex justify-center'>
+            <Image src={LoginBg} alt='Login BG' className='rounded-l-md hidden md:block'/>
+            <div className="w-full flex flex-col max-w-md p-4 md:p-6 rounded-r-md bg-gray-100 text-gray-900 shadow-2xl">
+                <div className="mb-2 md:mb-8 text-center">
+                    <h1 className="my-2 md:my-3 text-2xl md:text-4xl font-bold text-primary">Login</h1>
                 </div>
                 <form className="space-y-12">
                     <div className="space-y-4">
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
-                            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 rounded-md bg-[#FDF3F7] text-gray-900" />
+                            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 rounded-md bg-gray-300 text-gray-900 focus:outline-none" />
                         </div>
                         <div>
                             <div className="flex justify-between mb-2">
                                 <label htmlFor="password" className="text-sm">Password</label>
                                 <Link href="/" className="text-md  hover:text-primary">Forgot password?</Link>
                             </div>
-                            <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 rounded-md bg-[#FDF3F7] text-gray-900" />
+                            <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 rounded-md bg-gray-300 text-gray-900 focus:outline-none" />
                         </div>
                     </div>
                     <div className="space-y-2">
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -45,29 +45,29 @@ const RegisterPage = () => {
         }
     };
     return (
-        <div className='w-10/12 mx-auto my-10 flex justify-center'>
-            <Image src={LoginBg} alt='Login BG' className='rounded-l-md' />
-            <div className="w-full flex flex-col max-w-md p-6 rounded-r-md sm:p-10 bg-white text-gray-900 shadow-2xl">
-                <div className="mb-8 text-center">
-                    <h1 className="my-3 text-4xl font-bold">Register</h1>
+        <div className='w-11/12 md:w-10/12 mx-auto my-5 md:my-10 flex justify-center'>
+            <Image src={LoginBg} alt='Login BG' className='rounded-l-md hidden md:block' />
+            <div className="w-full flex flex-col max-w-md p-4 md:p-6 rounded-r-md bg-gray-100 text-gray-900 shadow-2xl">
+                <div className="mb-2 md:mb-8 text-center">
+                    <h1 className="my-2 md:my-3 text-2xl md:text-4xl font-bold text-primary">Register</h1>
                 </div>
                 <form onSubmit={(e) => handleUserRegistration(e)} className="space-y-12">
                     <div className="space-y-4">
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
-                            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 rounded-md bg-[#FDF3F7] text-gray-900" />
+                            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 rounded-md text-gray-900 bg-gray-300 focus:outline-none" />
                         </div>
                         <div>
                             <label htmlFor="fullname" className="block mb-2 text-sm">Full Name</label>
-                            <input type="text" name="fullname" id="fullname" placeholder="Leroy Jenkins" className="w-full px-3 py-2 rounded-md bg-[#FDF3F7] text-gray-900" />
+                            <input type="text" name="fullname" id="fullname" placeholder="Leroy Jenkins" className="w-full px-3 py-2 rounded-md text-gray-900 bg-gray-300 focus:outline-none" />
                         </div>
                         <div>
                             <label htmlFor="password" className="text-sm">Password</label>
-                            <input type="password" name="password" id="password" placeholder="***************" className="w-full px-3 py-2 rounded-md bg-[#FDF3F7] text-gray-900" />
+                            <input type="password" name="password" id="password" placeholder="***************" className="w-full px-3 py-2 rounded-md text-gray-900 bg-gray-300 focus:outline-none" />
                         </div>
                         <div>
                             <label htmlFor="profileImage" className="text-sm">Select Profile Image</label>
-                            <input type="file" name="profileImage" id="profileImage" className="w-full px-3 py-2 rounded-md bg-[#FDF3F7] text-gray-900" />
+                            <input type="file" name="profileImage" id="profileImage" className="w-full px-3 py-2 rounded-md text-gray-900 bg-gray-300 focus:outline-none" />
                         </div>
                     </div>
                     <div className="space-y-2">
