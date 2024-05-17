0c307984736af6d0ec3abf6e9db0a430eae1c6ec
saadhossain <mshsarkar@gmail.com>
2024-05-17 14:58:59 +0600

    db string modified

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,3 +1,3 @@
-const {DB_USER, PASSWORD} = process.env;
-// console.log(DB_USER, PASSWORD);
-export const mongoUrl = `mongodb+srv://${DB_USER}:${PASSWORD}@favfood.0y9cqwd.mongodb.net/favfood?retryWrites=true&w=majority`;
+// const {DB_USER, PASSWORD} = process.env;
+// // console.log(DB_USER, PASSWORD);
+export const mongoUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@favfood.0y9cqwd.mongodb.net/favfood?retryWrites=true&w=majority`;
