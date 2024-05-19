ee7164caf3987a45491ffd1e333060d423aa11d8
saadhossain <mshsarkar@gmail.com>
2024-05-19 09:43:32 +0600

    Pages added for footer and linked

--- src/app/components/shared/Footer.tsx
+++ src/app/components/shared/Footer.tsx
@@ -1,37 +1,37 @@
 import Image from 'next/image';
-import FavFood from '/public/favfood-icon.png';
 import Link from 'next/link';
+import FavFood from '/public/favfood-icon.png';
 
 const Footer = () => {
     return (
         <footer className='w-full bg-gray-100 py-5'>
             <div className='w-11/12 md:w-10/12 mx-auto grid grid-cols-1 md:grid-cols-4 gap-10 justify-between'>
-                <Image src={FavFood} alt='FavFood' width={150} height={150} className='hidden md:block'/>
+                <Image src={FavFood} alt='FavFood' width={150} height={150} className='hidden md:block' />
                 <div>
                     <h4 className='text-lg font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Quick Links</h4>
                     <ul className='flex flex-col gap-3'>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/payment-policy'>Payment Policy</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/return-refund'>Return and Refund</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/terms-conditions'>Terms & Conditions</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/privacy-policy'>Privacy Policy</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/payment-policy'>Payment Policy</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/return-refund'>Return and Refund</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/terms-conditions'>Terms & Conditions</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/privacy-policy'>Privacy Policy</Link></li>
                     </ul>
                 </div>
                 <div>
                     <h4 className='text-lg font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>About FavFood</h4>
                     <ul className='flex flex-col gap-3'>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/careers'>Careers</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/press'>Press release</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/affiliate'>Affiliate Programs</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/partnership'>Partner with us</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/about'>About Us</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/careers'>Careers</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/press'>Press release</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/affiliate'>Affiliate Programs</Link></li>
                     </ul>
                 </div>
                 <div>
                     <h4 className='text-lg font-semibold text-primary border-l-4 border-primary pl-2 mb-5'>Customer Care</h4>
                     <ul className='flex flex-col gap-3'>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/help'>Help Center</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/cashback'>Cashback Offer</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/safety'>Safety & Security</Link></li>
-                        <li className='text-black hover:text-secondary duration-200'><Link href='/partnership'>FavFood Operations</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/help'>Help Center</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/cashback'>Cashback Offer</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/safety-security'>Safety & Security</Link></li>
+                        <li className='text-black hover:text-secondary duration-200'><Link href='/pages/partnership'>Partner with us</Link></li>
                     </ul>
                 </div>
             </div>
---
+++ src/app/pages/affiliate/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AffiliateProgram = () => {
+  return (
+    <div className='w-11/12 md:w-10/12 mx-auto my-5 md:my-10'>Affiliate Program</div>
+  )
+}
+
+export default AffiliateProgram
