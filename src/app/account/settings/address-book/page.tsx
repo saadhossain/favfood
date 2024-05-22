a42840aee39424dd93bf44ac366ac2b9f3fee6b7
saadhossain <mshsarkar@gmail.com>
2024-05-22 23:19:01 +0600

    Address displayed on Address book page

--- src/app/account/settings/address-book/page.tsx
+++ src/app/account/settings/address-book/page.tsx
@@ -4,7 +4,7 @@ import SubHeading from '@/app/components/shared/headings/SubHeading';
 import LoadingSpinner from '@/app/components/spinner/LoadingSpinner';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
-import { FaPlus } from 'react-icons/fa';
+import { FaMapMarkerAlt, FaPlus } from 'react-icons/fa';
 
 const AddressBook = () => {
   const { data: session } = useSession();
@@ -18,22 +18,37 @@ const AddressBook = () => {
     }
     getCountries();
   }, [openModal]);
-
+  console.log(session)
   return (
     <div>
       <SubHeading heading={'Address Book'} />
       {
         !session ? <LoadingSpinner /> : <div className='w-full md:flex flex-col items-center gap-4 mt-5 md:mt-10'>
-          {/* Default Address */}
-          <SubHeading heading={'No Address Found! Please Add...'} />
-          {/* Add New Address */}
-          <div
-            onClick={() => setOpenModal(!openModal)}
-            className='w-full md:w-2/4 flex flex-col justify-center text-gray-800 border-2 border-dashed border-gray-800 rounded-md p-4 mt-2 md:mt-0 cursor-pointer h-28'>
-            <p className='flex items-center justify-center gap-2 text-lg'>
-              <FaPlus /> Add New Address
-            </p>
+          {session?.user.address ? <div className='w-full md:w-2/4 bg-gray-200 text-gray-700 font-semibold rounded-md p-4'>
+            <FaMapMarkerAlt className='w-5 h-5 mb-2 text-primary' />
+            <p>Street: {session?.user.address.streetAddress}</p>
+            <div className='flex items-center gap-5'>
+              <p>City: {session?.user.address.city}</p>
+              <p>State: {session?.user.address.state}</p>
+            </div>
+            <div className='flex items-center gap-5'>
+              <p>ZIP: {session?.user.address.zipCode}</p>
+              <p>Country: {session?.user.address.country}</p>
+            </div>
           </div>
+            : <>
+              <SubHeading heading={'No Address Found! Please Add...'} />
+              {/* Add New Address */}
+              <div
+                onClick={() => setOpenModal(!openModal)}
+                className='w-full md:w-2/4 flex flex-col justify-center text-gray-800 border-2 border-dashed border-gray-800 rounded-md p-4 mt-2 md:mt-0 cursor-pointer h-28'>
+                <p className='flex items-center justify-center gap-2 text-lg'>
+                  <FaPlus /> Add New Address
+                </p>
+              </div>
+            </>
+          }
+
         </div>
       }
       <AddAddressModal
