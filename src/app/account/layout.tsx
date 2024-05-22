3d4662733be4391071b52a12cb4625e4fc794543
saadhossain <mshsarkar@gmail.com>
2024-05-23 01:07:54 +0600

    user account pages done primarily

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -6,8 +6,10 @@ const MyAccountLayout = ({ children }: { children: ReactNode }) => {
     return (
         <div className='w-11/12 md:w-10/12 mx-auto my-3 md:my-10 flex gap-10'>
             <div className='w-1/5'>
-                <Heading heading={'My Account'} />
-                <MyAccountMenus />
+                <div className='sticky top-20 z-40'>
+                    <Heading heading={'My Account'} />
+                    <MyAccountMenus />
+                </div>
             </div>
             <main className='w-full md:w-9/12'>
                 {children}
--- src/app/components/ui/myaccount/MyAccountMenus.tsx
+++ src/app/components/ui/myaccount/MyAccountMenus.tsx
@@ -21,7 +21,7 @@ const MyAccountMenus = () => {
     );
 
     return (
-        <aside className='min-h-[62vh] bg-gray-100 p-4 rounded-md flex flex-col justify-between sticky top-10'>
+        <aside className='min-h-[62vh] bg-gray-100 p-4 rounded-md flex flex-col justify-between'>
             <nav>
                 <ul className='flex flex-col gap-1'>
                     <li className={getLinkClasses('/account')}>
