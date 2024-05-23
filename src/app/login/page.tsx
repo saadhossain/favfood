5748866a951a0571e0474f7872696d2e4b8aabbb
saadhossain <mshsarkar@gmail.com>
2024-05-23 13:18:17 +0600

    Redirect user after login based on session and callback

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,39 +3,60 @@ import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useContext, useEffect, useState } from 'react';
 import { FaEye, FaEyeSlash, FaGoogle } from "react-icons/fa";
 import Processing from '../components/spinner/Processing';
 import { DataContext } from '../context/DataContext';
 import { DataContextType } from '../types/DataContextTypes';
 import LoginBg from '/public/login-bg.jpg';
+
 const LoginPage = () => {
     const { data: session } = useSession();
-    // console.log(session);
+
+    //Get the callbackEndpoint from the url
+    const currentPath = window.location.href;
+
+    let callBackEndpoint = null;
+    if (currentPath.includes('callbackUrl')) {
+        const encodedCallbackUrl = currentPath.split('callbackUrl=')[1];
+        callBackEndpoint = decodeURIComponent(encodedCallbackUrl.split('3000')[1]);
+    }
+
+    // State for processing status
     const [processing, setProcessing] = useState(false);
     const { showPassword, setShowPassword } = useContext(DataContext) as DataContextType;
+
+    // Handle login form submission
     const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
         setProcessing(true);
         e.preventDefault();
         const form = e.target as HTMLFormElement;
         const email: string = form.email.value;
         const password: string = form.password.value;
-        // console.log(email, password);
+
         try {
-            signIn('credentials', {
-                email: email,
-                password: password,
-                redirect: false
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
             });
+            setProcessing(false);
         } catch (error: any) {
             setProcessing(false);
             throw new Error(error.message);
         }
     };
-    //If user logged in then redirect to account page
-    if (session) {
-        redirect('/account');
-    };
+    // Use useEffect to handle redirection based on session and callbackUrl
+    useEffect(() => {
+        if (session) {
+            if (callBackEndpoint) {
+                redirect(`${callBackEndpoint}`);
+            } else {
+                redirect('/account');
+            }
+        }
+    }, [session, callBackEndpoint]);
+
     return (
         <div className='w-11/12 md:w-10/12 mx-auto my-5 md:my-10 flex justify-center'>
             <Image src={LoginBg} alt='Login BG' className='rounded-l-md hidden md:block' />
@@ -43,9 +64,7 @@ const LoginPage = () => {
                 <div className="mb-2 md:mb-8 text-center">
                     <h1 className="my-2 md:my-3 text-2xl md:text-4xl font-bold text-primary">Login</h1>
                 </div>
-                <form
-                    onSubmit={(e) => handleLogin(e)}
-                    className="space-y-12">
+                <form onSubmit={handleLogin} className="space-y-12">
                     <div className="space-y-4">
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
@@ -54,32 +73,24 @@ const LoginPage = () => {
                         <div className='relative'>
                             <div className="flex justify-between mb-2">
                                 <label htmlFor="password" className="text-sm">Password</label>
-                                <Link href="/login" className="text-md  hover:text-primary">Forgot password?</Link>
+                                <Link href="/login" className="text-md hover:text-primary">Forgot password?</Link>
                             </div>
                             <input type={`${showPassword ? 'password' : 'text'}`} name="password" id="password" placeholder="***********" className="w-full px-3 py-2 rounded-md bg-gray-300 text-gray-900 focus:outline-none" />
-                            {/* Eye button for hide and show password */}
-                            <div
-                            onClick={() => setShowPassword(!showPassword)}
-                            className='cursor-pointer absolute top-11 right-2'
-                            >
-                                {
-                                    showPassword ? <FaEye /> : <FaEyeSlash />
-                                }
+                            <div onClick={() => setShowPassword(!showPassword)} className='cursor-pointer absolute top-11 right-2'>
+                                {showPassword ? <FaEye /> : <FaEyeSlash />}
                             </div>
                         </div>
                     </div>
                     <div className="space-y-2">
                         <div>
-                            <button type="submit" className="w-full flex items-center justify-center px-8 py-3 font-semibold rounded-md bg-primary text-white">{processing ? <Processing title={'Processing'} /> : 'Login'}</button>
+                            <button type="submit" className="w-full flex items-center justify-center px-8 py-3 font-semibold rounded-md bg-primary text-white">
+                                {processing ? <Processing title={'Processing'} /> : 'Login'}
+                            </button>
                         </div>
                     </div>
                 </form>
                 <div className='w-full my-3'>
-                    <button
-                        className='w-full flex gap-2 items-center justify-center bg-gray-400 text-white py-3 rounded-md font-semibold'
-                        onClick={() => signIn('google')}
-                        disabled
-                    >
+                    <button className='w-full flex gap-2 items-center justify-center bg-gray-400 text-white py-3 rounded-md font-semibold' onClick={() => signIn('google')} disabled>
                         <FaGoogle className='w-6 h-6' />Login with Google
                     </button>
                 </div>
