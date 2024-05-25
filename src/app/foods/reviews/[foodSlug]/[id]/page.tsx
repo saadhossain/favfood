aa159404efe909cbd4409e8b17d34d98fa60afdd
saadhossain <mshsarkar@gmail.com>
2024-05-26 02:36:19 +0600

    Loading Spinner Added on reviews pages

--- src/app/foods/reviews/[foodSlug]/[id]/page.tsx
+++ src/app/foods/reviews/[foodSlug]/[id]/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 import Heading from '@/app/components/shared/headings/Heading'
+import LoadingSpinner from '@/app/components/spinner/LoadingSpinner'
 import ReviewCard from '@/app/components/ui/reviews/ReviewCard'
+import { DataContext } from '@/app/context/DataContext'
+import { DataContextType } from '@/app/types/DataContextTypes'
 import { ReviewData } from '@/app/types/DataTypes'
 import { fetchReviewData } from '@/app/utils/fetchReviewData'
+import { useContext } from 'react'
 
 type Props = {
   params: {
@@ -13,6 +17,10 @@ type Props = {
 
 const SingleFoodReviews = ({ params }: Props) => {
   const reviews = fetchReviewData('foodsReview', params.id);
+  const { loading } = useContext(DataContext) as DataContextType;
+  if (loading) {
+    return <LoadingSpinner />
+  }
   return (
     <div className='w-11/12 md:w-10/12 mx-auto my-2 md:my-5'>
       <Heading heading={'Food Reviews'} />
