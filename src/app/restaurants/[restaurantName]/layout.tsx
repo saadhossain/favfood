fc223c171a1063b93aa59d99b25a877a8c1238ef
saadhossain <mshsarkar@gmail.com>
2024-05-31 00:08:50 +0600

    Single Food and Single Restaurant image broken issue fixed

--- src/app/restaurants/[restaurantName]/layout.tsx
+++ src/app/restaurants/[restaurantName]/layout.tsx
@@ -1,4 +1,5 @@
 'use client'
+import LoadingSpinner from '@/app/components/spinner/LoadingSpinner';
 import { RestaurantCard } from '@/app/components/ui/restaurant/RestaurantCard';
 import { RestaurantData } from '@/app/types/DataTypes';
 import { fetchSingleRestaurantData } from '@/app/utils/fetchSingleRestaurantData';
@@ -14,23 +15,30 @@ interface LayoutProps {
 const SingleRestaurantLayout: React.FC<LayoutProps> = ({ children, params }) => {
     const singleRestaurant: RestaurantData = fetchSingleRestaurantData(params.restaurantName);
     const activePath = usePathname();
+    if (!singleRestaurant._id) {
+        return <LoadingSpinner />
+    }
     return (
-        <div className={`w-11/12 md:w-10/12 mx-auto my-3 md:my-10`}>
-            <RestaurantCard restaurant={singleRestaurant} />
+        <div className='w-11/12 md:w-10/12 mx-auto my-3 md:my-10'>
+            {
+                singleRestaurant._id && <div>
+                    <RestaurantCard restaurant={singleRestaurant} />
 
-            {/* Restaurant Tab */}
-            <div className='w-full flex items-center gap-20 text-lg font-semibold mt-3 md:mt-8'>
-                <Link
-                    href={`/restaurants/${params.restaurantName}`}
-                    className={`border-b-4 ${!activePath.includes('reviews') ? 'border-primary': 'border-gray-200'} hover:border-primary pb-2 px-5`}
-                >Foods</Link>
-                <Link
-                    href={`/restaurants/${params.restaurantName}/reviews/${singleRestaurant._id}`}
-                    className={`border-b-4 ${activePath.includes('reviews') ? 'border-primary': 'border-gray-200'} hover:border-primary pb-2 px-5`}
-                >Reviews</Link>
-            </div>
-            <hr className='border-b-4 border-gray-200 -mt-1' />
-            {children}
+                    {/* Restaurant Tab */}
+                    <div className='w-full flex items-center gap-20 text-lg font-semibold mt-3 md:mt-8'>
+                        <Link
+                            href={`/restaurants/${params.restaurantName}`}
+                            className={`border-b-4 ${!activePath.includes('reviews') ? 'border-primary' : 'border-gray-200'} hover:border-primary pb-2 px-5`}
+                        >Foods</Link>
+                        <Link
+                            href={`/restaurants/${params.restaurantName}/reviews/${singleRestaurant._id}`}
+                            className={`border-b-4 ${activePath.includes('reviews') ? 'border-primary' : 'border-gray-200'} hover:border-primary pb-2 px-5`}
+                        >Reviews</Link>
+                    </div>
+                    <hr className='border-b-4 border-gray-200 -mt-1' />
+                    {children}
+                </div>
+            }
         </div>
     )
 }
--- src/app/restaurants/[restaurantName]/page.tsx
+++ src/app/restaurants/[restaurantName]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import LoadingSpinner from '@/app/components/spinner/LoadingSpinner';
 import ProductLoader from '@/app/components/spinner/ProductLoader';
 import FoodCard from '@/app/components/ui/homecomp/FoodCard';
 import { DataContext } from '@/app/context/DataContext';
@@ -15,12 +14,12 @@ interface Props {
 
 const page = ({ params }: Props) => {
   const { loading } = useContext(DataContext) as DataContextType;
-  const restaurantName = decodeURIComponent(params.restaurantName); 
+  const restaurantName = decodeURIComponent(params.restaurantName);
   const foods = fetchFoodData();
   const restaurantFoods = foods.filter((food: FoodData) => food.restaurant_Name.toLocaleLowerCase() === restaurantName);
-    if (loading) {
-        return <ProductLoader />;
-    }
+  if (loading) {
+    return <ProductLoader />;
+  }
   return (
     <div className='grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-5 my-5'>
       {
