fb4a96966c880ae0dd32e11b0e3a35a447b0b343
saadhossain <mshsarkar@gmail.com>
2024-08-01 13:55:20 +0600

    Metadata for wishlist added

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,38 +1,19 @@
-'use client';
-import Link from 'next/link';
-import { useContext } from 'react';
+import { Metadata } from 'next';
 import Heading from '../components/shared/headings/Heading';
-import TableSkeletonLoader from '../components/spinner/TableSkeletonLoader';
-import WishlistCard from '../components/ui/wishlist/WishlistCard';
-import { DataContext } from '../context/DataContext';
-import { setWishlistProducts } from '../lib/features/wishlistSlice';
-import { DataContextType } from '../types/DataContextTypes';
-import { FoodData } from '../types/DataTypes';
-import { getDataFromLocalStorage } from '../utils/getDataFromLocalStorage';
-import { getProductsInWishlist } from '../utils/getProductsInWishlist';
+import DisplayWishlists from '../components/ui/wishlist/DisplayWishlists';
+
+export const metadata: Metadata = {
+    title: 'Wishlist - FavFood',
+    description: 'Discover our innovative web app designed for food lovers! Easily purchase and order your favorite foods, add items to your wishlist, and search for delicious options. Leave feedback and enjoy a seamless e-commerce experience. Perfect for food enthusiasts seeking convenience and variety in one place.'
+}
 
 const Wishlist = () => {
-    const { loading } = useContext(DataContext) as DataContextType;
-    // Get products from localstorage and set them to setWishlistInLocalStorage state
-    getDataFromLocalStorage('favFoodWishlist', setWishlistProducts);
-    //Get all wishlist products in the cart
-    const productsInWishlist = getProductsInWishlist();
     return (
         <div className='w-11/12 md:w-10/12 mx-auto my-3 md:my-10 md:flex gap-10 justify-between'>
             {/* Wishlist Items */}
             <div className='w-full md:w-10/12'>
                 <Heading heading={'Wishlist'} />
-                {
-                    (loading) ? <TableSkeletonLoader /> :
-                        <div className='rounded-md grid grid-cols-2 md:block gap-2 md:gap-0'>
-                            {
-                                productsInWishlist?.map((product: FoodData | any) => <WishlistCard key={product?._id} product={product} />)
-                            }
-                        </div>
-                }
-                {
-                    (productsInWishlist?.length <= 0 && !loading) && <h1 className='w-full text-lg font-semibold text-gray-800'>There is not Product in the Wishlist. <Link href='/' className='text-primary'>Go to Shop</Link></h1>
-                }
+                <DisplayWishlists />
             </div>
         </div>
     );
