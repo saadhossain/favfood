8d704779af4a56a5b00b5550ea924f05d8cce6ca
saadhossain <mshsarkar@gmail.com>
2024-11-29 22:18:14 +0600

    unnecessary console removed

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -11,7 +11,6 @@ export const GET = async () => {
 
 export const POST = async (request: NextRequest) => {
     const orderData = await request.json();
-    console.log(orderData);
     await mongoose.connect(mongoUrl);
     const order = new Orders(orderData);
     const result = await order.save();
