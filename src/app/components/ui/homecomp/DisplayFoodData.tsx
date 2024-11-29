5245de2a19780a511d8ae2f8a39a885b77776219
saadhossain <mshsarkar@gmail.com>
2024-11-29 20:51:43 +0600

    set category tab to url, fetch data using redux

--- src/app/components/ui/homecomp/DisplayFoodData.tsx
+++ src/app/components/ui/homecomp/DisplayFoodData.tsx
@@ -1,28 +1,29 @@
+'use client'
 import FoodCard from '@/app/components/ui/homecomp/FoodCard';
+import { useGetDataQuery } from '@/app/lib/features/api/apiSlice';
 import { FoodData } from '@/app/types/DataTypes';
-import { fetchFoodDataByQuery } from '@/app/utils/fetchFoodDataByQuery';
 import { useState } from 'react';
 import ProductLoader from '../../spinner/ProductLoader';
 
-interface Props {
-    tabQuery: string, loading: boolean
-}
-const DisplayFoodData = ({ tabQuery, loading }: Props) => {
+const DisplayFoodData = ({ searchParams }: { searchParams: { category: string | undefined } }) => {
     const [page, setPage] = useState(1);
+    const category = searchParams.category || 'all-food'
     //Get the foods data...
-    const foods = fetchFoodDataByQuery(tabQuery, page);
-    if (loading) {
+    const { data: foods, isLoading } = useGetDataQuery(`/foods?tabQuery=${category}&page=${page}&limit=12`);
+
+    //Return a Loading Skeleton while fetching the data
+    if (isLoading) {
         return <ProductLoader />;
     }
     return (
         <div className='flex flex-col items-center justify-center'>
             <div className='grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-5 my-5'>
                 {
-                    foods.map((food: FoodData) => <FoodCard key={food._id} food={food} />)
+                    foods?.map((food: FoodData) => <FoodCard key={food._id} food={food} />)
                 }
             </div>
             <button
-                className={`bg-primary text-white hover:bg-secondary py-2 px-5 rounded-md ${foods.length <= 0 && 'hidden'}`}
+                className={`bg-primary text-white hover:bg-secondary py-2 px-5 rounded-md ${!foods || foods.length <= 0 && 'hidden'}`}
                 onClick={() => setPage(prevPage => prevPage + 1)}
             >
                 Load More
